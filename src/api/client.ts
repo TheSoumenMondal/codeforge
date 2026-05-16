79b8041a0b81c0d06126414e35a207c86b4f6728
TheSoumenMondal <thesoumenmondal50@gmail.com>
2026-05-16 14:23:46 +0530

    add axios headers

--- src/api/client.ts
+++ src/api/client.ts
@@ -4,6 +4,12 @@ import axios from "axios";
 const apiClient = axios.create({
   // biome-ignore lint/style/useNamingConvention: false positive
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+  headers: process.env.NEXT_PUBLIC_API_BASE_URL?.includes("ngrok")
+    ? {
+        "ngrok-skip-browser-warning": "true",
+      }
+    : {},
 });
 
 const queryClient = new QueryClient();
